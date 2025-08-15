50e469b395918c1cf98f4389c778b60c867a570b
Muhammad Bilal <m.bilal0111@gmail.com>
2025-08-16 00:03:07 +0500

    remove cover letter microservice

--- apps/api-gateway/src/cover-letter/cover-letter.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoverLetterService } from './cover-letter.service';
-import { CoverLetterController } from './cover-letter.controller';
-
-@Module({
-  controllers: [CoverLetterController],
-  providers: [CoverLetterService],
-})
-export class CoverLetterModule {}
